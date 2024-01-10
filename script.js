6c36726e952757bbf3d09802c7ccd525a3ab0363
PabloQB <qb.pablo.2000@gmail.com>
2024-01-10 22:35:37 +0100

    Random shuffle instead of random choice

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
 let cards = null;
+let idx = 0;
 
 $( document ).ready(function() {
     $.getJSON("info.json", function(json) {
-        cards = json["cards"];
+        cards = shuffle(json["cards"]);
+        change();
     });
 });
 
 function choose(choices) {
-var index = Math.floor(Math.random() * choices.length);
-return choices[index];
+    var index = Math.floor(Math.random() * choices.length);
+    return choices[index];
+}
+
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
 }
 
 function change() {
-    card = choose(cards);
+    let card = cards[idx % cards.length];
+    
     $('.my-card h3').html(card["name"]);
     $('.my-card img').attr("src", "imgs/"+card["file"]);
     $('.my-card ol').empty();
     $.each(card["list"], function(index, item) {
         $('.my-card ol').append('<li>' + item + '</li>');
     });
+
+    idx++;
 }
